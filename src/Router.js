17eec5856488fd5921c89f7d35340c64d8cb3ba0
Pradhana Indra <pradhana.indra@tokopedia.com>
2019-06-19 16:43:25 +0700

    Give wrapper to Router
    That will help if we decided to make the content horizontal rather than vertical.

--- src/Router.js
+++ src/Router.js
@@ -15,45 +15,57 @@ import Checkbox from './Checkbox';
 import CheckboxUncontrolled from './CheckboxUncontrolled';
 import Radio from './Radio';
 import RadioUncontrolled from './RadioUncontrolled';
+import OnClickSubmit from './OnClickSubmit';
+import NoForm from './NoForm';
 
 const Router = () => {
     return (
         <BrowserRouter>
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/controlled">Controlled</Link></li>
-                <li><Link to="/uncontrolled">Uncontrolled</Link></li>
-                <li><Link to="/textarea">Textarea</Link></li>
-                <li><Link to="/textarea-uncontrolled">Textarea (Uncontrolled)</Link></li>
-                <li><Link to="/select">Select</Link></li>
-                <li><Link to="/select-uncontrolled">Select (Uncontrolled)</Link></li>
-                <li><Link to="/select-multiple">Select - Multiple</Link></li>
-                <li><Link to="/select-multiple-uncontrolled">Select - Multiple (Uncontrolled)</Link></li>
-                <li><Link to="/file">File</Link></li>
-                <li><Link to="/file-uncontrolled">File (Uncontrolled)</Link></li>
-                <li><Link to="/checkbox">Checkbox</Link></li>
-                <li><Link to="/checkbox-uncontrolled">Checkbox (Uncontrolled)</Link></li>
-                <li><Link to="/radio">Radio</Link></li>
-                <li><Link to="/radio-uncontrolled">Radio (Uncontrolled)</Link></li>
-            </ul>
+            <div>
 
-            <hr />
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/controlled">Controlled</Link></li>
+                    <li><Link to="/uncontrolled">Uncontrolled</Link></li>
+                    <li><Link to="/textarea">Textarea</Link></li>
+                    <li><Link to="/textarea-uncontrolled">Textarea (Uncontrolled)</Link></li>
+                    <li><Link to="/select">Select</Link></li>
+                    <li><Link to="/select-uncontrolled">Select (Uncontrolled)</Link></li>
+                    <li><Link to="/select-multiple">Select - Multiple</Link></li>
+                    <li><Link to="/select-multiple-uncontrolled">Select - Multiple (Uncontrolled)</Link></li>
+                    <li><Link to="/file">File</Link></li>
+                    <li><Link to="/file-uncontrolled">File (Uncontrolled)</Link></li>
+                    <li><Link to="/checkbox">Checkbox</Link></li>
+                    <li><Link to="/checkbox-uncontrolled">Checkbox (Uncontrolled)</Link></li>
+                    <li><Link to="/radio">Radio</Link></li>
+                    <li><Link to="/radio-uncontrolled">Radio (Uncontrolled)</Link></li>
+                    <li><Link to="/onclick-submit">OnClick Submit</Link></li>
+                    <li><Link to="/no-form">{`No <form>`}</Link></li>
+                </ul>
 
-            <Route exact path="/" component={Home} />
-            <Route path="/controlled" component={Controlled} />
-            <Route path="/uncontrolled" component={Uncontrolled} />
-            <Route path="/textarea" component={Textarea} />
-            <Route path="/textarea-uncontrolled" component={TextareaUncontrolled} />
-            <Route path="/select" component={Select} />
-            <Route path="/select-uncontrolled" component={SelectUncontrolled} />
-            <Route path="/select-multiple" component={SelectMultiple} />
-            <Route path="/select-multiple-uncontrolled" component={SelectMultipleUncontrolled} />
-            <Route path="/file" component={FileForm} />
-            <Route path="/file-uncontrolled" component={FileUncontrolled} />
-            <Route path="/checkbox" component={Checkbox} />
-            <Route path="/checkbox-uncontrolled" component={CheckboxUncontrolled} />
-            <Route path="/radio" component={Radio} />
-            <Route path="/radio-uncontrolled" component={RadioUncontrolled} />
+                <hr />
+
+                <div>
+                    <Route exact path="/" component={Home} />
+                    <Route path="/controlled" component={Controlled} />
+                    <Route path="/uncontrolled" component={Uncontrolled} />
+                    <Route path="/textarea" component={Textarea} />
+                    <Route path="/textarea-uncontrolled" component={TextareaUncontrolled} />
+                    <Route path="/select" component={Select} />
+                    <Route path="/select-uncontrolled" component={SelectUncontrolled} />
+                    <Route path="/select-multiple" component={SelectMultiple} />
+                    <Route path="/select-multiple-uncontrolled" component={SelectMultipleUncontrolled} />
+                    <Route path="/file" component={FileForm} />
+                    <Route path="/file-uncontrolled" component={FileUncontrolled} />
+                    <Route path="/checkbox" component={Checkbox} />
+                    <Route path="/checkbox-uncontrolled" component={CheckboxUncontrolled} />
+                    <Route path="/radio" component={Radio} />
+                    <Route path="/radio-uncontrolled" component={RadioUncontrolled} />
+                    <Route path="/onclick-submit" component={OnClickSubmit} />
+                    <Route path="/no-form" component={NoForm} />
+                </div>
+
+            </div>
         </BrowserRouter>
     )
 }
