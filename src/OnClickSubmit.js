909736ee010fd0eda7d9c02a5e4f8dffb7106710
Pradhana Indra <pradhana.indra@tokopedia.com>
2019-06-19 16:44:48 +0700

    Add case for form that using onClick on submit button
    Rather than using onSubmit on form.

---
+++ src/OnClickSubmit.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+
+const Register = () => {
+    const [fullName, setFullName] = useState('');
+
+    const updateFullName = (event) => {
+        setFullName(event.target.value)
+    }
+
+    const submit = (event) => {
+        event.preventDefault();
+
+        fetch('https://api.example.com/login', {method: 'POST', body: JSON.stringify({name: fullName})})
+    }
+
+    return (
+        <React.Fragment>
+            <h1>Register (Submit by clicking button)</h1>
+            <form>
+                <label>
+                    Full Name:
+                    <input type="text" value={fullName} onChange={updateFullName} />
+                </label>
+                <button type="submit" onClick={submit}>Submit</button>
+            </form>
+        </React.Fragment>
+    )
+}
+
+export default Register
