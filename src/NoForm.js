9a3a1f1995831958c93485e609353d4d2caabed5
Pradhana Indra <pradhana.indra@tokopedia.com>
2019-06-19 16:45:14 +0700

    Add case for Form without `<form>`

---
+++ src/NoForm.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+
+const Register = () => {
+    const [fullName, setFullName] = useState('');
+
+    const updateFullName = (event) => {
+        setFullName(event.target.value)
+    }
+
+    const submit = (event) => {
+        event.preventDefault();
+
+        fetch('https://api.example.com/login', {method: 'POST', body: JSON.stringify({name: fullName})})
+    }
+
+    return (
+        <React.Fragment>
+            <h1>{`Register (No <form>)`}</h1>
+            <label>
+                Full Name:
+                <input type="text" value={fullName} onChange={updateFullName} />
+            </label>
+            <button type="submit" onClick={submit}>Submit</button>
+        </React.Fragment>
+    )
+}
+
+export default Register
