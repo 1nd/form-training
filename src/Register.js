ca1334073a6f724339936e756037d628aede4517
Pradhana Indra <pradhana.indra@tokopedia.com>
2019-06-27 16:01:32 +0700

    Add comment about Register form

--- src/Register.js
+++ src/Register.js
@@ -7,3 +7,16 @@ const Register = () => {
 }
 
 export default Register
+
+// Create a Register Form (complete with validation).
+
+// The form has following fields (all are required fields):
+    // Full name.
+    // Email.
+    // Password.
+    // Phone number.
+    // Profile picture.
+    // Gender.
+    // Address.
+    // Country (List at least 4 countries).
+    // Checkmark that user agree to Terms and Conditions.
